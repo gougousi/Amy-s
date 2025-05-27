d22fa97f639ce25654a67493c96a71bd50ff4a82
ami <anna.m.kal4@gmail.com>
2025-05-27 13:56:34 +0300

    popular

--- πληροφοριακα συστηματα παγκοσμιου ιστου/static/js/popular_products.js
+++ πληροφοριακα συστηματα παγκοσμιου ιστου/static/js/popular_products.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:5000/';
+const BASE_URL = 'http://127.0.0.1:3000/';
 
 async function fetchProducts() {
     try {
@@ -42,26 +42,19 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-
-    if (slides.length === 0) return;
-
-    if (n > slides.length) slideIndex = 1;
-    if (n < 1) slideIndex = slides.length;
-
-    for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-
-    for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-
     slides[slideIndex - 1].style.display = "block";
-    if (dots[slideIndex - 1]) {
     dots[slideIndex - 1].className += " active";
-    }
 }
 
 fetchProducts();
